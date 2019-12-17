6fef0c79f4a70f62a4fcfc01ae9a3acebea39f59
Derek <derekhaefner@gmail.com>
2019-12-16 18:49:23 -0600

    move form to custom popup

--- public/app.js
+++ public/app.js
@@ -1,13 +1,10 @@
 //Define map to allow access to the rest of the project
-let map;
+let map, popup, Popup;
 let new_marker_lat, new_marker_lng;
 
 // Get Elements
 const new_marker_form = document.getElementById('new-marker-form'),
-      map_display = document.getElementById('map-display'),
-      fieldName = document.getElementById('fieldName'),
-      sport = document.getElementById('sport'),
-      description = document.getElementById('description');
+      map_display = document.getElementById('map-display');
 
 //Google Maps link calls to initalize the map
 function initMap() {
@@ -37,19 +34,36 @@ function initMap() {
 
          //Call function to load markers from database
         getData();
+        
     }
 
-    //Listener to click and add Lat/Long to form and create new marker
     google.maps.event.addListener(map, 'click', (e) => {
+        const content = document.getElementById('content');
+        content.style.display = 'block';
+        if (window.innerWidth < 600) {
+            map.setCenter({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+        }
+        Popup = createPopupClass();
+        popup = new Popup(
+            new google.maps.LatLng(e.latLng.lat(), e.latLng.lng()),
+            content);
+        popup.setMap(map);
 
-        //Show form when user clicks the map
-        new_marker_form.style.display = 'block';
-        map_display.style.display = 'none';
-
-        //Set input for the Lat and Long where user clicks
         new_marker_lat = e.latLng.lat();
         new_marker_lng = e.latLng.lng();
     })
+
+    // //Listener to click and add Lat/Long to form and create new marker
+    // google.maps.event.addListener(map, 'click', (e) => {
+
+    //     //Show form when user clicks the map
+    //     new_marker_form.style.display = 'block';
+    //     map_display.style.display = 'none';
+
+    //     //Set input for the Lat and Long where user clicks
+    //     new_marker_lat = e.latLng.lat();
+    //     new_marker_lng = e.latLng.lng();
+    // })
 
     document.getElementById('recenter').addEventListener('click', () => {
         map.setCenter( {
@@ -114,57 +128,157 @@ async function getData() {
 }
 
 //Listener to submit new field/marker
-document.getElementById('submit').addEventListener('click', async e => {
+// document.getElementById('submit').addEventListener('click', async e => {
 
-    //Set values to an objest
-    const data = { 
-        fieldName: fieldName.value, 
-        sport: sport.value, 
-        description: description.value,
-        lat: new_marker_lat, 
-        lon: new_marker_lng 
-    };
+//     //Set values to an objest
+//     const data = { 
+//         fieldName: fieldName.value, 
+//         sport: sport.value, 
+//         description: description.value,
+//         lat: new_marker_lat, 
+//         lon: new_marker_lng 
+//     };
+
+//     //Check inputs value to see if they are filled in
+//     if (fieldName !== '') {
+//         try {
+//             // await fetch('/field_api/', options);
+//             const ajax = new XMLHttpRequest();
+//             ajax.open('POST', '/field_api');
+//             ajax.setRequestHeader('Content-Type', 'application/json');
+//             ajax.send(JSON.stringify(data));
+//         } catch (error) {
+//             console.log('error');
+//         }
+//     }; 
+
+//     //Hide form
+//     new_marker_form.style.display = 'none';
+//     map_display.style.display = 'block';
     
-    //Set options to submit to api
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    };
+//     // Function fetches via GET and loads all markers
+//     getData();
 
-    //Check inputs value to see if they are filled in
+//     //Clear values from inputs
+//     fieldName.value = '';
+//     sport.value = '';
+    
+//     e.preventDefault();
+// });
+
+//Listener to cancel new field
+// document.getElementById('cancel').addEventListener('click', () => {
+//     fieldName.value = '';
+//     sport.value = '';
+//     new_marker_form.style.display = 'none';
+//     map_display.style.display = 'block';
+// });
+
+function createPopupClass() {
+    /**
+     * A customized popup on the map.
+     * @param {!google.maps.LatLng} position
+     * @param {!Element} content The bubble div.
+     * @constructor
+     * @extends {google.maps.OverlayView}
+     */
+    function Popup(position, content) {
+      this.position = position;
+  
+      content.classList.add('popup-bubble');
+  
+      // This zero-height div is positioned at the bottom of the bubble.
+      var bubbleAnchor = document.createElement('div');
+      bubbleAnchor.classList.add('popup-bubble-anchor');
+      bubbleAnchor.appendChild(content);
+  
+      // This zero-height div is positioned at the bottom of the tip.
+      this.containerDiv = document.createElement('div');
+      this.containerDiv.classList.add('popup-container');
+      this.containerDiv.appendChild(bubbleAnchor);
+  
+      // Optionally stop clicks, etc., from bubbling up to the map.
+      google.maps.OverlayView.preventMapHitsAndGesturesFrom(this.containerDiv);
+    }
+    // ES5 magic to extend google.maps.OverlayView.
+    Popup.prototype = Object.create(google.maps.OverlayView.prototype);
+  
+    /** Called when the popup is added to the map. */
+    Popup.prototype.onAdd = function() {
+      this.getPanes().floatPane.appendChild(this.containerDiv);
+    };
+  
+    /** Called when the popup is removed from the map. */
+    Popup.prototype.onRemove = function() {
+      if (this.containerDiv.parentElement) {
+        this.containerDiv.parentElement.removeChild(this.containerDiv);
+      }
+    };
+  
+    /** Called each frame when the popup needs to draw itself. */
+    Popup.prototype.draw = function() {
+      var divPosition = this.getProjection().fromLatLngToDivPixel(this.position);
+  
+      // Hide the popup when it is far out of view.
+      var display =
+          Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000 ?
+          'block' :
+          'none';
+  
+      if (display === 'block') {
+        this.containerDiv.style.left = divPosition.x + 'px';
+        this.containerDiv.style.top = divPosition.y + 'px';
+      }
+      if (this.containerDiv.style.display !== display) {
+        this.containerDiv.style.display = display;
+      }
+    };
+  
+    return Popup;
+  }
+
+  document.getElementById('enter').addEventListener('click', () => {
+      handleSubmit();
+  })
+
+  document.getElementById('close').addEventListener('click', () => {
+    handleClose();
+});
+
+const handleSubmit = function() {
+    const newMarkerData = {
+        fieldName: document.getElementById('fieldName').value,
+        sport: document.getElementById('sport').value,
+        description: document.getElementById('description').value,
+        lat: new_marker_lat,
+        lon: new_marker_lng
+    }
+
     if (fieldName !== '') {
         try {
-            // await fetch('/field_api/', options);
             const ajax = new XMLHttpRequest();
             ajax.open('POST', '/field_api');
             ajax.setRequestHeader('Content-Type', 'application/json');
-            ajax.send(JSON.stringify(data));
+            ajax.send(JSON.stringify(newMarkerData));
         } catch (error) {
             console.log('error');
         }
     }; 
 
-    //Hide form
-    new_marker_form.style.display = 'none';
-    map_display.style.display = 'block';
-    
-    // Function fetches via GET and loads all markers
+    handleClose();
     getData();
+}
 
-    //Clear values from inputs
-    fieldName.value = '';
-    sport.value = '';
-    
-    e.preventDefault();
-});
-
-//Listener to cancel new field
-document.getElementById('cancel').addEventListener('click', () => {
-    fieldName.value = '';
-    sport.value = '';
-    new_marker_form.style.display = 'none';
-    map_display.style.display = 'block';
-});
+const handleClose = function() {
+    popup.setMap(null);
+    let newDiv = document.createElement('div');
+    newDiv.id = 'content';
+    newDiv.innerHTML = '<label>Field Name</label><br><input type="text" name="fieldName" id="fieldName"><br><label>Sport</label><br><select id="sport" name="sport"><option value="soccer">Soccer</option><option value="football">Football</option><option value="baseball">Baseball</option><option value="basketball">Basketball</option></select><br><input type="text" id="description" name="description"><br><button id="enter" class="btn">Enter</button><button id="close" class="btn">Close</button>';
+    map_display.appendChild(newDiv);
+    document.getElementById('close').addEventListener('click', () => {
+        handleClose()
+    })
+    document.getElementById('enter').addEventListener('click', () => {
+        handleSubmit();
+    })
+}
